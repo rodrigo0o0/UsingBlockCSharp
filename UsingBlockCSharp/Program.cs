fde110596986f4711e464309a30b5b863726cc20
Rodrigo Sandeski <sandeski47@gmail.com>
2022-03-31 01:25:59 -0300

    first commit

---
+++ UsingBlockCSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+
+namespace UsingBlockCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string path = @"c:\temp\file1.txt";
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                    Console.ReadKey();
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
